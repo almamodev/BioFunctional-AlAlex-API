fae91efcd23dfeb45872ad0d4193df8452530c80
Albert Martín Moreno <almamodev@gmail.com>
2024-02-07 11:12:32 +0100

    updates

--- pathway.py
+++ pathway.py
@@ -4,8 +4,12 @@ import typing
 
 
 class Pathway:
-    @classmethod
-    def response(cls) -> str:
+    def __init__(self) -> None:
+        self.__html: bs4.BeautifulSoup = Pathway.__beautiful_soup()
+
+
+    @staticmethod
+    def __response() -> str:
         URL: str = 'https://www.kegg.jp/kegg/pathway.html'
 
         response: requests.Response = requests.get(URL)
@@ -13,55 +17,41 @@ class Pathway:
 
         return response.text
         
-
-    @classmethod
-    def beautiful_soup(cls) -> bs4.BeautifulSoup:
-        markup: str = cls.response()
+    
+    @staticmethod
+    def __beautiful_soup() -> bs4.BeautifulSoup:
+        markup: str = Pathway.__response()
         features: str = 'html.parser'
 
         return bs4.BeautifulSoup(markup, features)
     
 
-    @classmethod
-    def interactions(cls) -> typing.List[str]:
-        beautiful_soup: bs4.BeautifulSoup = cls.beautiful_soup()
-
+    def interactions(self) -> typing.List[str]:
         interactions: typing.List = []
 
-        for h4 in beautiful_soup.find_all('h4', attrs={'id': True}):
+        for h4 in self.__html.find_all('h4', attrs={'id': True}):
             interaction: str = h4.text.split('.')[0]
             interactions.append(interaction)
 
         return interactions
     
 
-    @classmethod
-    def reactions(cls) -> typing.List[str]:
-        beautiful_soup: bs4.BeautifulSoup = cls.beautiful_soup()
-
+    def reactions(self) -> typing.List[str]:
         reactions: typing.List = []
 
-        for b in beautiful_soup.find_all('b')[8:]:
+        for b in self.__html.find_all('b')[8:]:
             reaction: str = b.text.split()[0]
             reactions.append(reaction)
         
         return reactions
     
 
-    @classmethod
-    def relations(cls) -> typing.List[typing.List[str]]: 
-        beautiful_soup: bs4.BeautifulSoup = cls.beautiful_soup()
-
+    def relations(self) -> typing.List[typing.List[str]]: 
         relations: typing.List = []
 
-        for div in beautiful_soup.find_all('div', attrs={'class': 'list'}):
-            for relation in div.text.split('\n'):
-                if relation != '':
-                    print(relation)
-
+        for div in self.__html.find_all('div', attrs={'class': 'list'}):
+            relation: typing.List[str] = [relation[:5] for relation in div.text.split('\n') if relation != '']
+            relations.append(relation)
+            
         return relations
-
-
-
-Pathway.relations()
-
+    
