4634d701211e1a94ac4ba3ab13bd9e276a65b654
almamodev <almamodev@gmail.com>
2024-02-08 20:27:39 +0000

    updates

--- test.py
+++ test.py
@@ -1,22 +1,20 @@
 from pathway import Pathway
-import pandas
 import typing
 
-df = pandas.read_csv('dataset.csv')
 
-def mapping(collection: Pathway.collection, df: pandas.DataFrame, column: str):
-    for pathway in collection:
-        for _, row in df.iterrows():
-            if row[column][2:] in pathway['relation']:
-                df['interaction_entry'] = pathway['interaction']['entry']
-                df['interaction_name'] = pathway['interaction']['name']
-                df['reaction_entry'] = pathway['reaction']['entry']
-                df['reaction_name'] = pathway['reaction']['name']
-    df.to_csv('dataset_filtrado.csv')
-
- 
-pathway = Pathway()
-mapping(pathway.collection(), df, 'ONTOLOGY')
+def update(
+    pathway: Pathway, 
+    records: typing.List[typing.Dict[str, typing.Any]], 
+    key: str
+) -> typing.List[typing.Dict[str, typing.Any]]:
+    for pathway in pathway.collection():
+        for record in records:
+            if record[key][2:] == pathway['relation']:
+                record['interaction_entry'] = pathway['interaction']['entry']
+                record['reaction_entry'] = pathway['reaction']['entry']
+                record['interaction_name'] = pathway['interaction']['name']
+                record['reaction_name'] = pathway['reaction']['name']
+    return records
 
 
     
